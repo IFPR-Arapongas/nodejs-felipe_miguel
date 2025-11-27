5a6241d87d589b04a0a02b7602c9a0b27ba0b2b0
FelipeZanatta09 <felipezanattacouto01102009@gmail.com>
2025-11-26 21:37:08 -0300

    Felipe

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -2,11 +2,12 @@ document.querySelector('#adicionar').addEventListener('click', (e) => {
     e.preventDefault()
     const texto = document.querySelector('#nome').value
     const texto2 = document.querySelector('#marca').value
+    const texto3 = document.querySelector('#modelo').value
 
     const conteudo = document.querySelector('#conteudo')
     const div = document.createElement('div')
     const p = document.createElement('p')
-    p.textContent = `Nome: ${texto} / Marca: ${texto2}.`
+    p.textContent = `Nome: ${texto}. Marca: ${texto2}. Modelo: ${texto3}`
     div.append(p)
     conteudo.append(div)
 })
