fd4011b55352612f228f5a951601a0731be6f2a7
magualitos <miguel.sambati25@gmail.com>
2025-11-26 21:55:11 -0300

    fdpwgh

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 import express from 'express'
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
 
 const app = express()
 app.use(express.json());
+app.use(cookieParser());
+app.use(express.static('public'));
+
+dotenv.config();
 
 const db = await mysql.createConnection({
     host: 'localhost',
@@ -37,6 +44,48 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
     console.log("Servidor rodando em http://localhost:3000");
 });
+
+function autenticarToken(req, res, next) {
+    const token = req.cookies['authToken'];
+  
+    if (!token)
+      return res.status(401).send({ auth: false, mensagem: 'Nenhum token informado' })
+  
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.email = decoded.email;
+      next();
+    } catch (err) {
+      return res.status(401).send({
+        auth: false,
+        mensagem: 'Token inválido ou expirado.'
+      });
+    }
+  }
+  
+  app.post('/login', async (req, res) => {
+    let { email, senha } = req.body;
+  
+    if (email != 'teste@gmail.com' || senha !== '123456') {
+      return res.status(401).send({
+        auth: false,
+        mensagem: 'Usuário ou senha inválidos'
+      })
+    }
+  
+    let token = jwt.sign(
+      { email: email },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
+  
+    res.cookie('authToken', token, {
+      httpOnly: true, 
+      maxAge: 24 * 3600000, 
+      sameSite: 'Lax'
+    });
+    res.status(200).send({ auth: true, message: 'Login realizado com sucesso' })
+})
 
 
 
